73a4c743d906445cdd0c0d86a9fabc27035c6440
M-1-hashim <mohammadhashimgholami8@gmail.com>
2026-09-20 14:45:00 +0430

    Add files via upload

---
+++ android/app/src/main/java/com/hashem/musicplayer/PlaybackService.java
@@ -0,0 +1,32 @@
+package com.hashem.musicplayer;
+
+import androidx.media3.common.MediaItem;
+import androidx.media3.exoplayer.ExoPlayer;
+import androidx.media3.session.MediaSession;
+import androidx.media3.session.MediaSessionService;
+
+public class PlaybackService extends MediaSessionService {
+    private MediaSession mediaSession = null;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ExoPlayer player = new ExoPlayer.Builder(this).build();
+        mediaSession = new MediaSession.Builder(this, player).build();
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mediaSession != null) {
+            mediaSession.getPlayer().release();
+            mediaSession.release();
+            mediaSession = null;
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public MediaSession onGetSession(MediaSession.ControllerInfo controllerInfo) {
+        return mediaSession;
+    }
+}
